d0bf4b4bb2baf55c688e286925dd5ad0349bed2c
Pablo Kaniefsky <pablokan@gmail.com>
2023-06-05 23:10:40 -0300

    *carga

--- guias/guia05/g05e07.py
+++ guias/guia05/g05e07.py
@@ -1,20 +1,20 @@
 #Almacenar en dos  listas paralelas, nombres y sexos de 8 personas.
 #Al finalizar, recorrerlas y mostrar los nombres de las mujeres. Dos funciones: carga y mostrarMujeres
-nombres = []
-sexos = []
-def carga(nombre, sexo):
+def carga():
+    nombres = []
+    sexos = []
     for datos in range(3):
         nombre = input("ingrese nombre: ")
         nombres.append(nombre)
-        sexo = input("ingrese sexo (F/M): ")
+        sexo = input("ingrese sexo (f/m): ")
         sexos.append(sexo)
+    return nombres, sexos
 
-def mostrarMujeres():
+def mostrarMujeres(nombres, sexos):
     femenino = []
     for i in range (len(sexos)):
         if sexos[i] == "f":
             femenino.append(nombres[i])
     return femenino
 
-carga(nombres, sexos)
-print(mostrarMujeres())
+print(mostrarMujeres(*carga()))
