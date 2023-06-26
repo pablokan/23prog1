9dbd793b9743e34489ab58d1328332c6a6b4a47a
Pablo Kaniefsky <pablokan@gmail.com>
2023-06-26 16:57:43 -0300

    pr3

---
+++ archivos/pr3.py
@@ -0,0 +1,20 @@
+def carga():
+    from calculo_edad import edad
+    lista = []
+    with open('archivos/personas.txt') as archPersonas:
+        archPersonas.readline()
+        for linea in archPersonas:
+            fecha = linea.split()[1]
+            e = edad(fecha) 
+            if  e >= 18:
+                l = linea.split()[0] + " " + str(e) + '\n'
+                lista.append(l)
+    return lista
+
+def salida(lista):
+    with open('mayores.txt', 'w') as archMayores:
+        archMayores.write('Nombre, Edad\n')
+        archMayores.writelines(lista)
+
+salida(carga())
+
