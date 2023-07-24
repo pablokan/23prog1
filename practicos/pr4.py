49deb1fce5e3da14a32d6a41763a4139621c0a0e
Pablo Kaniefsky <pablokan@gmail.com>
2023-07-24 17:31:28 -0300

    pr4 #1

---
+++ practicos/pr4.py
@@ -0,0 +1,10 @@
+nacidos2008 = "Eva,17039,f,Daniel,19005,m,Emily,17434,f,Emma,18813,f,Ethan,20216,m,Julia,18616,f,Jacob,22594,m,Joshua,19205,m,Michael,20626,m,Olivia,17081,f"
+
+nacidos2018 = { 
+    'varones': [[], ['Liam', 19837], ['Noah', 18267], ['Michael', 14516], ['James', 13525], ['Oliver', 13389],]
+    
+}
+
+
+
+
